313b82dbd81ab60673991b5f6dcb9d391833fdc3
Safaitul Islam Sefat <59335251+safaitulsefat@users.noreply.github.com>
2024-05-15 22:12:38 +0600

    learn cookies and session

---
+++ ninth_project/first_app/views.py
@@ -0,0 +1,44 @@
+from datetime import datetime,timedelta
+from django.shortcuts import render
+from django.http import HttpResponse
+# Create your views here.
+def home(request):
+    response = render(request,'home.html')
+    response.set_cookie('name','rahim')
+    #response.set_cookie('name','karim', max_age =10)
+    response.set_cookie('name','karim', expires=datetime.utcnow()+timedelta(days=7))
+    return response
+
+def get_cookie(request):
+    name = request.COOKIES.get('name')
+    return render(request,'get_cookie.html',{'name':name})
+
+def delete_cookie(request):
+    response = render(request,'del.html')
+    response.delete_cookie('name')
+    return response
+
+def set_session(request):
+    # data = {
+    #     'name': 'rahim',
+    #     'age': 23,
+    #     'language':'Bangla'
+    # }
+    # print(request.session.get_session_cookie_age())
+    # print(request.session.get_expiry_date())
+    request.session['name'] = 'sefat'
+    return render(request,'home.html')
+
+def get_session(request):
+    if 'name' in request.session:  
+        name = request.session.get('name')
+        request.session.modified = True
+        return render(request,'get_session.html',{'name':name})
+    else:
+        return HttpResponse('Your session has been expired.Login AGAIN')
+
+def delete_session(request):
+    #del request.session['name']
+    request.session.flush()
+    request.session.clear_expired()
+    return render(request,'del.html')
